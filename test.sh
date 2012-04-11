99e8454d7fe54fee89f8c93dec80c40cb77ce8b5
Epoch Wolf <epochwolf@gmail.com>
2012-04-10 21:40:35 -0700

    Adding a script to check for broken links.

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+wget -e robots=off --spider -r -l 8 -o wget_errors.txt http://localhost:4000
+
+EXIT_CODE=$?
+if [ $EXIT_CODE -gt 0 ]; then
+    echo "ERROR: Found broken link(s):"
+    egrep '^http://localhost:4000/' wget_errors.txt
+    exit 1
+else
+    echo "PASS"
+    rm ./wget_errors.txt
+fi
